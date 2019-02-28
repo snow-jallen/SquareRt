cfa395fc9499f4207b7df52cba0d748f36169033
Jonathan Allen <jallen@snow.edu>
2019-02-28 16:02:38 -0700

    Add project files.

---
+++ SquareRt.Core.Tests/SquareRtCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace SquareRt.Core.Tests
+{
+    [TestFixture]
+    public class SquareRtCalculatorTests
+    {
+        ISquareRtCalculator calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Arrange
+            calc = new SquareRtCalculator();
+        }
+
+        [Test]
+        public void Calculate_4_Returns2()
+        {
+            // Act
+            var squareRoot = calc.Calculate(4);
+            // Assert
+            Assert.AreEqual(2, squareRoot);
+        }
+    }
+}
